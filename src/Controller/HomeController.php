0bb32643d1c334bc4e7fbcf4075cfe5c292bdca6
Svetlana Muraveckaja-Odincova <svetlana.mur.85@gmail.com>
2018-04-15 21:16:28 +0300

    App latest changes from kickstart project

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return $this->render('home/index.html.twig', [
+
+        return $this->render('base.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
