0656d17eb9fcfaa555e3825f9deaaf95c8113cdd
Egliote <egliote@outlook.com>
2018-04-21 17:01:42 +0300

    Egles registration form and other changes merged with Svetlanas previous commits.

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class AdminController extends Controller
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/review", name="review")
+     */
+    public function review(UserInterface $user = null)
+    {
+        if (!$user) {
+            throw new AccessDeniedException('Your user is not authorised');
+        }
+        if (in_array('ROLE_MENTORIUS', $user->getRoles())) {
+            return $this->render('admin/review.html.twig');
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            return $this->render('admin/plan.html.twig');
+        }
+        // Design system to not reach this line: produce HTTP 500 response
+        throw new AccessDeniedException('User not authorised by known roles');
+    }
+}
