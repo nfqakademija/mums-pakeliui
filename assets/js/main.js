5b1e911f5348e19b6418d1c59835f834d61a7141
Egliote <egliote@outlook.com>
2018-05-06 16:53:33 +0300

    Fixed base page layout (jumpy footer).

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,37 +10,3 @@ $(document).ready(function(){
         }
     });
 });
-
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){
-        $('header').addClass("sticky");
-    }
-    else{
-        $('header').removeClass("sticky");
-    }
-});
-
-var h_hght = 81;
-var h_mrg = 0;
-
-$(function(){
-
-    var elem = $('#top_nav');
-    var top = $(this).scrollTop();
-
-    if(top > h_hght){
-        elem.css('top', h_mrg);
-    }
-
-    $(window).scroll(function(){
-        top = $(this).scrollTop();
-
-        if (top+h_mrg < h_hght) {
-            elem.css('top', (h_hght-top));
-        } else {
-            elem.css('top', h_mrg);
-        }
-    });
-
-});
