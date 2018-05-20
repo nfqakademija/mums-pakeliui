4044e27188b529e4f97d30de1cad5f84c8efcd3c
Egliote <egliote@outlook.com>
2018-05-20 11:30:26 +0300

    Symfony test fill search form in home page, submit it and go to search results page.

--- tests/HomeControllerTest.php
+++ tests/HomeControllerTest.php
@@ -3,15 +3,26 @@
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Goutte\Client;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testHomePageFormAndHeaderButtons()
+
+    public function testHomePageSubmitSearchFormButton()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $crawler1 = $client->request('GET', '/');
 
         $this->assertSame(200, $client->getResponse()->getStatusCode());
-        $this->assertSame(1, $crawler->filter('.search-form')->count());
+        $this->assertSame(1, $crawler1->filter('.search-form')->count());
+        $form = $crawler1->selectButton('trip_search_filter')->form();
+        $client->submit($form, array('trip_search[travelerType]' => '0'));
+
+
+        $redirectPage = $client->getCrawler();
+        echo $redirectPage->getUri();
+        $this->assertSame(1, $redirectPage->filter('html:contains("Filtruoti")')->count());
+
     }
+
 }
