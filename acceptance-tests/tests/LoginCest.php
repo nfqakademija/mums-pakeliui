7423163afbbeff70ac9ef5c651a6f1f9dc4b01b1
Egliote <egliote@outlook.com>
2018-05-21 15:28:56 +0300

    Corrected selenium test (user registration, login with valid, login with invalid).

--- acceptance-tests/tests/LoginCest.php
+++ acceptance-tests/tests/LoginCest.php
@@ -1,6 +1,36 @@
 <?php
 class LoginCest 
 {
+    public function registration(AcceptanceTester $I)
+    {
+        $I->amOnPage('/');
+        $I->wait(2);
+        $I->clickWithLeftButton('#dropdownMenuButton');
+        $I->wait(2);
+        $I->click('Registruotis');
+        $I->wait(2);
+        $I->fillField('El. paštas', "vasara@takas.lt");
+        $I->wait(2);
+        $I->fillField('Naudotojo vardas', "vasara");
+        $I->wait(2);
+        $I->fillField('Slaptažodis', "123");
+        $I->wait(2);
+        $I->fillField('Pakartoti slaptažodį', "123");
+        $I->wait(2);
+        $I->clickWithLeftButton('html.html-changeHeight.k-ff.k-ff60 body.page-look div.container.main div.row div.col-sm-12.col-md-4.col-lg-3.mx-auto.registration-form form.fos_user_registration_register div input.btn.btn-pink');
+        $I->wait(2);
+        $I->see('vasara');
+        $I->wait(2);
+        $I->clickWithLeftButton('#dropdownMenuButton');
+        $I->wait(2);
+        $I->click('Atsijungti');
+        $I->wait(2);
+        $I->dontSee('vasara');
+        $I->wait(2);
+    }
+    /**
+     * @depends registration
+     */
     public function loginSuccessfully(AcceptanceTester $I)
     {
         $I->amOnPage('/');
@@ -9,16 +39,19 @@ class LoginCest
         $I->wait(2);
         $I->click('Prisijungti');
         $I->wait(2);
-        $I->fillField('Naudotojo vardas', "asilas");
+        $I->fillField('Naudotojo vardas', "vasara");
         $I->wait(2);
         $I->fillField('Slaptažodis', "123");
         $I->wait(2);
         $I->clickWithLeftButton('#_submit');
         $I->wait(2);
-        $I->see('asilas');
+        $I->see('vasara');
         $I->wait(2);
     }
-    
+
+    /**
+     * @depends registration
+     */
     public function loginWithInvalidPassword(AcceptanceTester $I)
     {
         $I->amOnPage('/');
@@ -27,7 +60,7 @@ class LoginCest
         $I->wait(2);
         $I->click('Prisijungti');
         $I->wait(2);
-        $I->fillField('Naudotojo vardas', "asilas");
+        $I->fillField('Naudotojo vardas', "vasara");
         $I->wait(2);
         $I->fillField('Slaptažodis', "567");
         $I->wait(2);
