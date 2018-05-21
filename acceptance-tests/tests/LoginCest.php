54a0fba7e3d12583832be249a5668d2ff5e3f042
Egliote <egliote@outlook.com>
2018-05-21 12:33:32 +0300

    Selenium login with valid and with invalid passwords test added.

--- acceptance-tests/tests/LoginCest.php
+++ acceptance-tests/tests/LoginCest.php
@@ -1,18 +1,39 @@
 <?php
 class LoginCest 
-{    
-    public function _before(AcceptanceTester $I)
-    {
-        $I->amOnPage('/');
-    }
-
+{
     public function loginSuccessfully(AcceptanceTester $I)
     {
-        // write a positive login test 
+        $I->amOnPage('/');
+        $I->wait(2);
+        $I->clickWithLeftButton('#dropdownMenuButton');
+        $I->wait(2);
+        $I->click('Prisijungti');
+        $I->wait(2);
+        $I->fillField('Naudotojo vardas', "asilas");
+        $I->wait(2);
+        $I->fillField('Slaptažodis', "123");
+        $I->wait(2);
+        $I->clickWithLeftButton('#_submit');
+        $I->wait(2);
+        $I->see('asilas');
+        $I->wait(2);
     }
     
     public function loginWithInvalidPassword(AcceptanceTester $I)
     {
-        // write a negative login test
+        $I->amOnPage('/');
+        $I->wait(2);
+        $I->clickWithLeftButton('#dropdownMenuButton');
+        $I->wait(2);
+        $I->click('Prisijungti');
+        $I->wait(2);
+        $I->fillField('Naudotojo vardas', "asilas");
+        $I->wait(2);
+        $I->fillField('Slaptažodis', "567");
+        $I->wait(2);
+        $I->clickWithLeftButton('#_submit');
+        $I->wait(2);
+        $I->see('Klaidingi duomenys.');
+        $I->wait(2);
     }       
 }
